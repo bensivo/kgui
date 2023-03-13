cee5f3fd517f9d6aac1dd269eb2969b84fdf09d1
Ben Sivoravong <bensivo@gmail.com>
2023-03-12 20:39:21 -0500

    Nav styling

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,89 +1,19 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { nanoid } from 'nanoid';
-import { Observable } from 'rxjs';
-import { Consumer, ConsumerStore } from 'src/app/store/consumer.store';
 import { NavStore } from 'src/app/store/nav.store';
-import { Producer, ProducerStore } from 'src/app/store/producer.store';
-import { Tab, TabStore } from 'src/app/store/tab.store';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.less']
+  styleUrls: ['./nav.component.less'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavComponent {
   constructor(
-    private consumerStore: ConsumerStore,
-    private producerStore: ProducerStore,
     private navStore: NavStore,
-    private tabStore: TabStore,
   ) { }
 
   isCollapsed = false;
-
-  consumers$: Observable<Consumer[]> = this.consumerStore.store.entities$
-  producers$: Observable<Producer[]> = this.producerStore.store.entities$
-
-  onSelectConsumer(consumer: Consumer) {
-    // See if this consumer is already in a tab
-    const existing = this.tabStore.store.entities.find(t => 
-      t.targetType === 'consumer' && t.targetId === consumer.id
-    )
-    if (existing) {
-      this.tabStore.selectTab(existing.id)
-      return;
-    }
-
-    const tab: Tab = {
-      id: nanoid(),
-      active: true,
-      targetType: 'consumer',
-      targetId: consumer.id
-    };
-
-    this.tabStore.store.upsert(tab);
-    this.tabStore.selectTab(tab.id);
-  }
-
-  onSelectProducer(producer: Producer) {
-    const existing = this.tabStore.store.entities.find(t => 
-      t.targetType === 'producer' && t.targetId === producer.id
-    )
-    if (existing) {
-      this.tabStore.selectTab(existing.id)
-      return;
-    }
-    const tab: Tab = {
-      id: nanoid(),
-      active: true,
-      targetType: 'producer',
-      targetId: producer.id
-    };
-    this.tabStore.store.upsert(tab);
-    this.tabStore.selectTab(tab.id);
-  }
-
-  addConsumer() {
-    this.consumerStore.store.upsert({
-      id: nanoid(),
-      name: 'Untitled',
-      topic: '',
-      follow: false,
-      offset: 0,
-      filters: [],
-    });
-  }
-
-  addProducer() {
-    this.producerStore.store.upsert({
-      id: nanoid(),
-      name: 'Untitled',
-      topic: '',
-      partition: 0,
-      message: '',
-    });
-  }
 
   addFolder() {
     this.navStore.insertNode({
